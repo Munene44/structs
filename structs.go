e42fde17efa72bc3a63639f81a08dbc210de4a46
nenekelvin <karvinmitchar@gmail.com>
2019-10-21 12:11:11 +0300

    :books: construct new person struct

--- structs.go
+++ structs.go
@@ -3,5 +3,17 @@ package main
 import "fmt"
  
 type person struct {
-	
+	name string
+	age int
 }
+ func NewPerson(name string) *person {
+ 	p := person{name: name}
+ 	p.age = 42
+ 	return &p
+ }
+
+ fun main() {
+ 	fmt.Println(person{"Kim", 30})
+ 	fmt.Println(person{"john", 40})
+ 	f.mt.Println(&person{"lin", age 40})
+ }
